26b44fd4e1fef397701df1471515a0090a0b34c6
choi minju <chlminju32@gmail.com>
2025-11-09 02:45:09 +0900

    적 피해 애니메이션 추가

--- enemy.py
+++ enemy.py
@@ -147,26 +147,33 @@ class Attack:
 
 class Hurt:
     image = None
-    sizes = []
+    sizes =  [(0, 37), (123, 41), (249, 42)]
     def __init__(self, enemy):
         self.enemy = enemy
         if Hurt.image == None:
             Hurt.image = load_image('./적/남자1(근)/Hurt.png')
 
     def enter(self, e):
-        pass
+        self.enemy.dir = 1
 
     def exit(self, e):
         pass
 
     def do(self):
-        pass
+        self.enemy.frame = (self.enemy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 3
 
     def draw(self):
-        pass
+        left, width = Hurt.sizes[int(self.enemy.frame)]
+        bottom = 0
+        height = 67
+        if self.enemy.face_dir == -1:
+            Hurt.image.clip_composite_draw(left, bottom, width, height, 0, 'h', self.enemy.x, self.enemy.y, 120, 180)
+        else:
+            Hurt.image.clip_composite_draw(left, bottom, width, height, 0, '', self.enemy.x, self.enemy.y, 120, 180)
 
     def get_bb(self):
-        pass
+        return self.enemy.x - 35, self.enemy.y - 90, self.enemy.x + 35, self.enemy.y + 90
+
 
 class Dead:
     image = None
@@ -205,13 +212,15 @@ class Enemy:
         self.RUN = Run(self)
         self.WALK = Walk(self)
         self.ATTACK = Attack(self)
+        self.HURT = Hurt(self)
         self.state_machine = StateMachine(
-            self.ATTACK,
+            self.HURT,
             {
                 self.IDLE : {time_out: self.RUN},
                 self.RUN : {time_out: self.IDLE},
                 self.WALK : {time_out: self.IDLE},
-                self.ATTACK : {time_out: self.IDLE}
+                self.ATTACK : {time_out: self.IDLE},
+                self.HURT : {time_out: self.IDLE}
             }
         )
 
