74cadb5a2892ac9406087ba4f37ee3ca328b5263
choi minju <chlminju32@gmail.com>
2025-11-09 23:26:59 +0900

    gameclear_mode 객체 생성

---
+++ gameclear_mode.py
@@ -0,0 +1,33 @@
+# game_clear_mode.py
+
+from pico2d import *
+import game_framework
+import play_mode
+
+game_clear_image = None
+
+def init():
+    pass
+
+
+def finish():
+    pass
+
+
+def handle_events():
+    pass
+
+def update():
+    pass
+
+
+def draw():
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
