9b5abcd86b6af4604ef0a770614311217a5df679
choi minju <chlminju32@gmail.com>
2025-11-09 21:42:53 +0900

    적 자동 스폰 기능 추가

--- play_mode.py
+++ play_mode.py
@@ -11,6 +11,10 @@ from enemy import Enemy
 
 girl = None
 font = None
+spawn_timer = 0.0
+spawn_cooldown = 10.0
+spawn_count = 0
+max_spawn_count = 10
 
 def handle_events():
     event_list = get_events()
@@ -24,7 +28,7 @@ def handle_events():
 
 
 def init():
-    global girl, font
+    global girl, font, spawn_timer, spawn_count
 
     girl = Girl()
     girl.x = 800
@@ -33,17 +37,35 @@ def init():
 
     font = load_font('ENCR10B.TTF', 16)
 
-
     Subway('./배경/내부2.png', 800, 300, 1600, 600, 0, is_looping=True)
 
     enemy = Enemy(girl)
     game_world.add_object(enemy, 4)
     game_world.add_collision_pair('book:enemy', None, enemy)
     game_world.add_collision_pair('girl:enemy', None, enemy)
+    spawn_count = 1
+    spawn_timer = get_time()  # (get_time 함수)
 
 def update():
+    global spawn_timer, spawn_cooldown, spawn_count, max_spawn_count
     game_world.update()
     game_world.handle_collisions()
+
+    if spawn_count >= max_spawn_count:
+        return
+
+    current_time = get_time()
+    if current_time - spawn_timer > spawn_cooldown:
+        spawn_timer = current_time
+
+        enemy = Enemy(girl)
+        enemy.x = 1600 + 100
+
+        game_world.add_object(enemy, 4)
+        game_world.add_collision_pair('book:enemy', None, enemy)
+        game_world.add_collision_pair('girl:enemy', None, enemy)
+
+        spawn_count += 1
 
 
 def draw():
